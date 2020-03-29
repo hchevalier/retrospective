6609eed995e67c46414b24606333ce89bef48b37
Hugo Chevalier <hugo.chevalier@doctolib.com>
2020-03-30 00:13:50 +0200

    Add button to get invite URL

--- app/javascript/components/ParticipantsList.js
+++ app/javascript/components/ParticipantsList.js
@@ -1,7 +1,26 @@
 import React from 'react'
 import classNames from 'classnames'
+import Button from '@material-ui/core/Button'
 
 const ParticipantsList = ({ participants, profile }) => {
+  const copyUrlToClipboard = () => {
+    const toCopy = document.createElement('span')
+    toCopy.setAttribute('type', 'hidden')
+    toCopy.appendChild(document.createTextNode(window.location.href))
+    document.body.appendChild(toCopy);
+    const range = document.createRange()
+    const selection = window.getSelection()
+
+    range.selectNodeContents(toCopy)
+    selection.removeAllRanges()
+    selection.addRange(range)
+    document.execCommand('copy')
+    selection.removeAllRanges()
+
+    toCopy.remove()
+    alert('Copied invite URL to clipboard')
+  }
+
   return (
     <div id='participants-list'>
       {participants.map(({ surname, organizer, uuid }, index) => (
@@ -9,6 +28,7 @@ const ParticipantsList = ({ participants, profile }) => {
           {surname}
         </div>
       ))}
+      {profile?.organizer && <Button variant='contained' color='primary' onClick={copyUrlToClipboard}>+</Button>}
     </div>
   )
 }
