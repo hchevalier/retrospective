cc5f252463909c436fa72b30e78c2a4e1c59aef9
Hugo Chevalier <hugo.chevalier@doctolib.com>
2020-04-20 00:15:50 +0200

    Bump Ruby to 2.6.6 and add headless mode to tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -31,7 +31,7 @@ class ActionDispatch::IntegrationTest
   include AssertHelpers
 
   Capybara.server = :puma, { Silent: true }
-  Capybara.default_driver = :selenium_chrome
+  Capybara.default_driver = ENV.fetch('HEADLESS', false) == 'true' ? :selenium_chrome_headless : :selenium_chrome
   Capybara.default_max_wait_time = 5.seconds
 
   # Reset sessions and driver between tests
