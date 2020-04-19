c714b8cf42249f5b737be1efa634a136c02b3834
Moustapha Sall <moustapha.sall@doctolib.com>
2020-04-19 23:57:07 +0200

    Refactor httpClient

--- app/javascript/lib/httpClient.js
+++ app/javascript/lib/httpClient.js
@@ -1,32 +1,32 @@
-import axios from 'axios'
+import axios from "axios";
+const csrfToken = document.querySelector("[name=csrf-token]").content;
+const httpClient = axios.create({
+  headers: { "Content-Type": "application/json", "X-CSRF-TOKEN": csrfToken },
+});
 
-const defaultHeaders = {
-  'Content-Type': 'application/json'
-}
+export const post = async ({ url, payload = {}, headers = {} }) => {
+  try {
+    const response = await httpClient.post(url, payload, headers);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
 
-export const post = ({ url, payload = {}, headers = {}}) => {
-  const csrfToken = document.querySelector('[name=csrf-token]').content
-  axios.defaults.headers.common['X-CSRF-TOKEN'] = csrfToken
+export const put = async ({ url, payload = {}, headers = {} }) => {
+  try {
+    const response = await httpClient.put(url, payload, headers);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
 
-  return axios.post(url, payload, { ...defaultHeaders, ...headers })
-    .then(response => response.data)
-    .catch(error => error)
-}
-
-export const put = ({ url, payload = {}, headers = {} }) => {
-  const csrfToken = document.querySelector('[name=csrf-token]').content
-  axios.defaults.headers.common['X-CSRF-TOKEN'] = csrfToken
-
-  return axios.put(url, payload, { ...defaultHeaders, ...headers })
-    .then(response => response.data)
-    .catch(error => error)
-}
-
-export const destroy = ({ url, payload = {}, headers = {} }) => {
-  const csrfToken = document.querySelector('[name=csrf-token]').content
-  axios.defaults.headers.common['X-CSRF-TOKEN'] = csrfToken
-
-  return axios.delete(url, payload, { ...defaultHeaders, ...headers })
-    .then(response => response.data)
-    .catch(error => error)
-}
+export const destroy = async ({ url, payload = {}, headers = {} }) => {
+  try {
+    const response = await httpClient.delete(url, payload, headers);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
