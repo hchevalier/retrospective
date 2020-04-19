64aa136bfb7bb72f1d2e63703cb99bb07beb63f1
Moustapha Sall <moustapha.sall@doctolib.com>
2020-04-20 00:20:17 +0200

    Style change

--- app/javascript/lib/httpClient.js
+++ app/javascript/lib/httpClient.js
@@ -1,32 +1,33 @@
-import axios from "axios";
-const csrfToken = document.querySelector("[name=csrf-token]").content;
+import axios from "axios"
+
+const csrfToken = document.querySelector("[name=csrf-token]").content
 const httpClient = axios.create({
   headers: { "Content-Type": "application/json", "X-CSRF-TOKEN": csrfToken },
-});
+})
 
 export const post = async ({ url, payload = {}, headers = {} }) => {
   try {
-    const response = await httpClient.post(url, payload, headers);
-    return response.data;
+    const response = await httpClient.post(url, payload, headers)
+    return response.data
   } catch (error) {
-    throw error;
+    throw error
   }
-};
+}
 
 export const put = async ({ url, payload = {}, headers = {} }) => {
   try {
-    const response = await httpClient.put(url, payload, headers);
-    return response.data;
+    const response = await httpClient.put(url, payload, headers)
+    return response.data
   } catch (error) {
-    throw error;
+    throw error
   }
-};
+}
 
 export const destroy = async ({ url, payload = {}, headers = {} }) => {
   try {
-    const response = await httpClient.delete(url, payload, headers);
-    return response.data;
+    const response = await httpClient.delete(url, payload, headers)
+    return response.data
   } catch (error) {
-    throw error;
+    throw error
   }
-};
+}
