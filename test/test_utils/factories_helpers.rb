01d3b6d022e28a256e33b8517d107daeedfa67b5
Moustapha Sall <moustapha.sall@doctolib.com>
2020-04-21 23:21:47 +0200

    Restore tests factory helpers

--- test/test_utils/factories_helpers.rb
+++ test/test_utils/factories_helpers.rb
@@ -1,6 +1,26 @@
-# frozen_string_literal: true
-
 module FactoriesHelpers
+  def create_retrospective!(step: 'gathering', with_reflection: false, color: nil)
+    retrospective = Retrospective.create!(
+      name: 'Retrospective',
+      kind: 'glad_sad_mad',
+      step: step,
+      organizer_attributes: { surname: 'Organizer', email: 'organizer@yopmail.com' }
+    )
+    @organizer = retrospective.organizer
+    @organizer.update!(color: color) if color
+
+    if with_reflection
+      glad_zone = retrospective.zones.find_by(identifier: 'Glad')
+      @organizer.reflections.create!(zone: glad_zone, content: 'I am so glad!')
+    end
+
+    retrospective
+  end
+
+  def add_another_participant(retrospective, surname:, email:, color: nil)
+    retrospective.participants.create!(surname: surname, email: email, color: color)
+  end
+
   def write_reflection(zone: 'Mad', content: 'This is my reflection')
     click_on 'New reflection'
     fill_in 'content', with: content
