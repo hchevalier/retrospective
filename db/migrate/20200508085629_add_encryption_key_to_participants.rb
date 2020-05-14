1a5debfec3b96f2abb31ac2e20db0de26678fa75
Hugo Chevalier <hugo.chevalier@doctolib.com>
2020-05-14 23:38:46 +0200

    Update migration to handle existing participants

--- db/migrate/20200508085629_add_encryption_key_to_participants.rb
+++ db/migrate/20200508085629_add_encryption_key_to_participants.rb
@@ -1,5 +1,14 @@
 class AddEncryptionKeyToParticipants < ActiveRecord::Migration[6.0]
-  def change
-    add_column :participants, :encryption_key, :string, null: false
+  def up
+    add_column :participants, :encryption_key, :string, null: true
+    clear_cache!
+    Participant.where(encryption_key: nil).each do |participant|
+      participant.update_column(:encryption_key, (0...32).map { ('a'..'z').to_a[rand(26)] }.join)
+    end
+    change_column_null :participants, :encryption_key, false
+  end
+
+  def down
+    remove_column :participants, :encryption_key
   end
 end
