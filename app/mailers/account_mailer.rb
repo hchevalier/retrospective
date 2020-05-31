96db500d287bc879335f0fb01ac5ec8775ef1535
Hugo Chevalier <hugo.chevalier@doctolib.com>
2020-05-31 13:44:08 +0200

    Fix mail preview

--- app/mailers/account_mailer.rb
+++ app/mailers/account_mailer.rb
@@ -1,6 +1,8 @@
 class AccountMailer < ApplicationMailer
-  def send_password_reset(account:)
-    @account = account
+  def send_password_reset(account: nil)
+    @account = account || params&.fetch(:account, nil)
+    raise unless @account
+
     mail(to: @account.email, subject: 'Password reset')
   end
 end
