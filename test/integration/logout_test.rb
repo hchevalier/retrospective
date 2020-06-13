0a4f7cd96c8e1c1cc668c8e006bd6b5fc5841a2b
Jade Vandal <jade.vandal@doctolib.com>
2020-06-13 17:32:15 +0200

    Add a test

---
+++ test/integration/logout_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class LogoutTest < ActionDispatch::IntegrationTest
+  test 'logout successfully' do
+    account = create(:account)
+    as_user(account)
+
+    visit '/'
+    assert_text 'Dashboard'
+    click_on 'Log out'
+
+    assert_text 'Log in'
+  end
+end
