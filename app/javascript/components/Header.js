1f716fe443ec00b5d2b768dfff3d3343c9da8b87
jadevdl <37371042+jadevdl@users.noreply.github.com>
2020-06-13 20:58:13 +0200

    Add logout button (#47)
    * Add a route for logout and clear the session
    
    * Add button Logout into the navbar
    
    * Add a test
    
    * Remove signed cookie 🚮

--- app/javascript/components/Header.js
+++ app/javascript/components/Header.js
@@ -2,15 +2,18 @@ import React from 'react'
 import HomeIcon from 'images/home-icon.svg'
 
 const Header = () => (
-  <nav className='bg-gray-900 mb-6 shadow text-white' role='navigation'>
-    <div className='container mx-auto p-4 flex flex-wrap items-center md:flex-no-wrap'>
-      <div className="mr-4 md:mr-8">
-        <a href='/'>
-          <img src={HomeIcon} width="24" />
-        </a>
+<nav className='bg-gray-900 mb-6 shadow text-white' role='navigation'>
+  <div className='container mx-auto p-4 flex flex-wrap items-center md:flex-no-wrap'>
+    <div className='flex'>
+      <a href='/'>
+        <img src={HomeIcon} width="24" />
+      </a>
+      <span className='px-4'>Docto retro</span>
+    </div>
+    <div className='flex flex-grow'>
+      <div className='justify-end ml-auto'>
+        <a href='/logout'>Log out</a>
       </div>
-      <div className='mr-4 md:mr-8'>
-        Docto retro
       </div>
     </div>
   </nav>
