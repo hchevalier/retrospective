a1e960b2356a658d4bd229c361b45aff9210f909
Hugo Chevalier <hugo.chevalier@doctolib.com>
2020-06-25 09:04:37 +0200

    Redesign Glad Sad Mad

--- app/javascript/components/retrospectives/GladSadMad.js
+++ app/javascript/components/retrospectives/GladSadMad.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import { useSelector, shallowEqual } from 'react-redux'
 import PropTypes from 'prop-types'
+import classNames from 'classnames'
 import Zone from './Zone'
 import Icon from '../Icon'
+import './GladSadMad.scss'
 
 const GladSadMad = ({ highlightZones, onZoneClicked }) => {
   const reflections = useSelector(state => state.reflections.ownReflections, shallowEqual)
@@ -13,26 +15,34 @@ const GladSadMad = ({ highlightZones, onZoneClicked }) => {
   const mad = zones.find((zone) => zone.name === 'Mad')
 
   return (
-    <>
-      <Zone
-        reference={glad}
-        highlight={highlightZones}
-        icon={<Icon retrospectiveKind='glad_sad_mad' zone='Glad' dataAttributes={{ 'data-id': glad.id }} onClick={onZoneClicked} />}
-        reflections={reflections.filter((reflection) => reflection.zone.id === glad.id)}
-        onClick={onZoneClicked} />
-      <Zone
-        reference={sad}
-        highlight={highlightZones}
-        icon={<Icon retrospectiveKind='glad_sad_mad' zone='Sad' dataAttributes={{ 'data-id': sad.id }} onClick={onZoneClicked} />}
-        reflections={reflections.filter((reflection) => reflection.zone.id === sad.id)}
-        onClick={onZoneClicked} />
-      <Zone
-        reference={mad}
-        highlight={highlightZones}
-        icon={<Icon retrospectiveKind='glad_sad_mad' zone='Mad' dataAttributes={{ 'data-id': mad.id }} onClick={onZoneClicked} />}
-        reflections={reflections.filter((reflection) => reflection.zone.id === mad.id)}
-        onClick={onZoneClicked} />
-    </>
+    <div className='circle-outer'>
+      <ul className='circle'>
+        <li>
+          <Zone
+            reference={glad}
+            icon={<Icon retrospectiveKind='glad_sad_mad' zone='Glad' dataAttributes={{ 'data-id': glad.id }} onClick={onZoneClicked} />}
+            reflections={reflections.filter((reflection) => reflection.zone.id === glad.id)}
+            onClick={onZoneClicked} />
+          <div className={classNames('background', { 'highlight': highlightZones })} data-id={glad.id} onClick={onZoneClicked}></div>
+        </li>
+        <li>
+          <Zone
+            reference={sad}
+            icon={<Icon retrospectiveKind='glad_sad_mad' zone='Sad' dataAttributes={{ 'data-id': sad.id }} onClick={onZoneClicked} />}
+            reflections={reflections.filter((reflection) => reflection.zone.id === sad.id)}
+            onClick={onZoneClicked} />
+          <div className={classNames('background', { 'highlight': highlightZones })} data-id={sad.id} onClick={onZoneClicked}></div>
+        </li>
+        <li>
+          <Zone
+            reference={mad}
+            icon={<Icon retrospectiveKind='glad_sad_mad' zone='Mad' dataAttributes={{ 'data-id': mad.id }} onClick={onZoneClicked} />}
+            reflections={reflections.filter((reflection) => reflection.zone.id === mad.id)}
+            onClick={onZoneClicked} />
+          <div className={classNames('background', { 'highlight': highlightZones })} data-id={mad.id} onClick={onZoneClicked}></div>
+        </li>
+      </ul>
+    </div>
   )
 }
 
