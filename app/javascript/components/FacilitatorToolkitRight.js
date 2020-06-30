857056efe288146c8efebbacc28b768b6c7f0a47
Hugo Chevalier <hugo.chevalier@doctolib.com>
2020-06-30 09:14:16 +0200

    Hide next button during 'done' step

--- app/javascript/components/FacilitatorToolkitRight.js
+++ app/javascript/components/FacilitatorToolkitRight.js
@@ -3,11 +3,13 @@ import { useSelector } from 'react-redux'
 import Button from './Button'
 
 const FacilitatorToolkitRight = () => {
-  const channel = useSelector(state => state.orchestrator.subscription)
+  const { subscription: channel, step } = useSelector(state => state.orchestrator)
 
   const nextStep = () => {
     channel?.send({ intent: 'next' })
   }
+
+  if (step === 'done') return null
 
   return (
     <div className='flex flex-row'>
