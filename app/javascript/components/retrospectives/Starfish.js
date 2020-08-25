a951c51e22cd036fe990de4d1d9332ce339d85b4
Hugo Chevalier <hugo.chevalier@doctolib.com>
2020-08-25 03:53:45 +0200

    Improve Starfish layout (#101)

--- app/javascript/components/retrospectives/Starfish.js
+++ app/javascript/components/retrospectives/Starfish.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import { useSelector, shallowEqual } from 'react-redux'
+import classNames from 'classnames'
 import PropTypes from 'prop-types'
 import Zone from './Zone'
 import Icon from '../Icon'
+import './Starfish.scss'
 
 const Starfish = ({ highlightZones, onZoneClicked }) => {
   const reflections = useSelector(state => state.reflections.ownReflections, shallowEqual)
@@ -15,38 +17,50 @@ const Starfish = ({ highlightZones, onZoneClicked }) => {
   const less = zones.find((zone) => zone.name === 'Less')
 
   return (
-    <>
-      <Zone
-        reference={keep}
-        highlight={highlightZones}
-        icon={<Icon retrospectiveKind='starfish' zone='Keep' dataAttributes={{ 'data-id': keep.id }} onClick={onZoneClicked} />}
-        reflections={reflections.filter((reflection) => reflection.zone.id === keep.id)}
-        onClick={onZoneClicked} />
-      <Zone
-        reference={start}
-        highlight={highlightZones}
-        icon={<Icon retrospectiveKind='starfish' zone='Start' dataAttributes={{ 'data-id': start.id }} onClick={onZoneClicked} />}
-        reflections={reflections.filter((reflection) => reflection.zone.id === start.id)}
-        onClick={onZoneClicked} />
-      <Zone
-        reference={stop}
-        highlight={highlightZones}
-        icon={<Icon retrospectiveKind='starfish' zone='Stop' dataAttributes={{ 'data-id': stop.id }} onClick={onZoneClicked} />}
-        reflections={reflections.filter((reflection) => reflection.zone.id === stop.id)}
-        onClick={onZoneClicked} />
-      <Zone
-        reference={more}
-        highlight={highlightZones}
-        icon={<Icon retrospectiveKind='starfish' zone='More' dataAttributes={{ 'data-id': more.id }} onClick={onZoneClicked} />}
-        reflections={reflections.filter((reflection) => reflection.zone.id === more.id)}
-        onClick={onZoneClicked} />
-      <Zone
-        reference={less}
-        highlight={highlightZones}
-        icon={<Icon retrospectiveKind='starfish' zone='Less' dataAttributes={{ 'data-id': less.id }} onClick={onZoneClicked} />}
-        reflections={reflections.filter((reflection) => reflection.zone.id === less.id)}
-        onClick={onZoneClicked} />
-    </>
+    <div className='circle-outer'>
+      <ul className='circle'>
+        <li>
+          <Zone
+            reference={less}
+            icon={<Icon retrospectiveKind='starfish' zone='Less' dataAttributes={{ 'data-id': less.id }} onClick={onZoneClicked} />}
+            reflections={reflections.filter((reflection) => reflection.zone.id === less.id)}
+            onClick={onZoneClicked} />
+          <div className={classNames('background', { 'highlight': highlightZones })} data-id={less.id} onClick={onZoneClicked}></div>
+        </li>
+        <li>
+          <Zone
+            reference={keep}
+            icon={<Icon retrospectiveKind='starfish' zone='Keep' dataAttributes={{ 'data-id': keep.id }} onClick={onZoneClicked} />}
+            reflections={reflections.filter((reflection) => reflection.zone.id === keep.id)}
+            onClick={onZoneClicked} />
+          <div className={classNames('background', { 'highlight': highlightZones })} data-id={keep.id} onClick={onZoneClicked}></div>
+        </li>
+        <li>
+          <Zone
+            reference={more}
+            icon={<Icon retrospectiveKind='starfish' zone='More' dataAttributes={{ 'data-id': more.id }} onClick={onZoneClicked} />}
+            reflections={reflections.filter((reflection) => reflection.zone.id === more.id)}
+            onClick={onZoneClicked} />
+          <div className={classNames('background', { 'highlight': highlightZones })} data-id={more.id} onClick={onZoneClicked}></div>
+        </li>
+        <li>
+          <Zone
+            reference={start}
+            icon={<Icon retrospectiveKind='starfish' zone='Start' dataAttributes={{ 'data-id': start.id }} onClick={onZoneClicked} />}
+            reflections={reflections.filter((reflection) => reflection.zone.id === start.id)}
+            onClick={onZoneClicked} />
+          <div className={classNames('background', { 'highlight': highlightZones })} data-id={start.id} onClick={onZoneClicked}></div>
+        </li>
+        <li>
+          <Zone
+            reference={stop}
+            icon={<Icon retrospectiveKind='starfish' zone='Stop' dataAttributes={{ 'data-id': stop.id }} onClick={onZoneClicked} />}
+            reflections={reflections.filter((reflection) => reflection.zone.id === stop.id)}
+            onClick={onZoneClicked} />
+          <div className={classNames('background', { 'highlight': highlightZones })} data-id={stop.id} onClick={onZoneClicked}></div>
+        </li>
+      </ul>
+    </div>
   )
 }
 
