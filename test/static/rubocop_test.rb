62de24c062a5b48f03999091db3f4bcd833aab8e
Hugo Chevalier <hugo.chevalier@doctolib.com>
2020-11-05 07:39:09 +0100

    Use puts instead of Rails.logger

--- test/static/rubocop_test.rb
+++ test/static/rubocop_test.rb
@@ -4,7 +4,7 @@ require 'test_helper'
 
 class RubocopTest < ActiveSupport::TestCase
   test 'should have no rubocop error' do
-    Rails.logger.info('Checking for Rubocop offenses:')
+    puts 'Checking for Rubocop offenses:'
     output = system %(rubocop --cache false --config .rubocop.yml 2>&1)
     fail('Check failed: ' + output.to_s) if $CHILD_STATUS.exitstatus != 0
 
