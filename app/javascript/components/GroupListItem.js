293aa2020954847c7b604d64b2a5a75cd9cd7dc7
Joseph Rozencwajg <joseph@josephr.com>
2021-01-16 13:41:20 +0100

    update boolean props

--- app/javascript/components/GroupListItem.js
+++ app/javascript/components/GroupListItem.js
@@ -1,17 +1,20 @@
-import React, { Fragment } from 'react'
-import PropTypes from 'prop-types'
-import classNames from 'classnames'
-import { Link } from 'react-router-dom'
+import React, { Fragment } from "react"
+import PropTypes from "prop-types"
+import classNames from "classnames"
+import { Link } from "react-router-dom"
 
-const JOIN_BUTTON_LABEL = 'JOIN'
-const LEAVE_BUTTON_LABEL = 'LEAVE'
+const JOIN_BUTTON_LABEL = "JOIN"
+const LEAVE_BUTTON_LABEL = "LEAVE"
 
-export const GroupAccessListItem = ({ groupAccess, currentAccount, handleLeaveGroup }) => (
+export const GroupAccessListItem = ({
+  groupAccess,
+  currentAccount,
+  handleLeaveGroup,
+}) => (
   <GroupListItem
     groupAccessOrPendingInvitation={groupAccess}
     currentAccount={currentAccount}
     handleButtonClicked={handleLeaveGroup}
-    isPendingInvitation={false}
   />
 )
 
@@ -21,12 +24,16 @@ GroupAccessListItem.propTypes = {
   handleLeaveGroup: PropTypes.func,
 }
 
-export const PendingInvitationListItem = ({ pendingInvitation, currentAccount, handleJoinGroup }) => (
+export const PendingInvitationListItem = ({
+  pendingInvitation,
+  currentAccount,
+  handleJoinGroup,
+}) => (
   <GroupListItem
     groupAccessOrPendingInvitation={pendingInvitation}
     currentAccount={currentAccount}
     handleButtonClicked={handleJoinGroup}
-    isPendingInvitation={true}
+    isPendingInvitation
   />
 )
 
@@ -48,37 +55,50 @@ const GroupListItem = ({
 
   return (
     <WrapperComponent {...wrapperProps}>
-        <div className='p2 w-64'>
-        <div className='flex flex-col bg-gray-200 rounded-md p-2 m-2'>
-            <div className='border-b-2 border-gray-400'>
-            <span className='font-bold text-blue-800'>{group.name}</span>
-            </div>
+      <div className="p2 w-64">
+        <div className="flex flex-col bg-gray-200 rounded-md p-2 m-2">
+          <div className="border-b-2 border-gray-400">
+            <span className="font-bold text-blue-800">{group.name}</span>
+          </div>
 
-            <div className='mt-1 text-sm'>{group.membersCount} members</div>
-            <div className='mt-2 text-sm'>Created on {new Date(group.createdAt).toLocaleDateString()}</div>
-            <div className='mt-1 text-sm'>
-              {isPendingInvitation ? 'Invited' : 'Joined'} on {new Date(groupAccessOrPendingInvitation.createdAt).toLocaleDateString()}
-            </div>
-            <div className='mt-2 text-sm'>{group.pendingTasksCount} actions pending</div>
-            <div className='mt-1 text-sm'>{group.allTimeRetrospectivesCount} retrospectives since creation</div>
+          <div className="mt-1 text-sm">{group.membersCount} members</div>
+          <div className="mt-2 text-sm">
+            Created on {new Date(group.createdAt).toLocaleDateString()}
+          </div>
+          <div className="mt-1 text-sm">
+            {isPendingInvitation ? "Invited" : "Joined"} on{" "}
+            {new Date(
+              groupAccessOrPendingInvitation.createdAt
+            ).toLocaleDateString()}
+          </div>
+          <div className="mt-2 text-sm">
+            {group.pendingTasksCount} actions pending
+          </div>
+          <div className="mt-1 text-sm">
+            {group.allTimeRetrospectivesCount} retrospectives since creation
+          </div>
 
-            <div className='flex flex-grow mt-4 items-end self-end'>
+          <div className="flex flex-grow mt-4 items-end self-end">
             {currentAccount && (
-                <button
-                className={classNames('text-xxs inline-flex items-center font-bold leading-sm uppercase px-3 py-1 rounded-full', {
-                  'bg-blue-200 text-blue-700': isPendingInvitation,
-                  'bg-red-200 text-red-700': !isPendingInvitation,
-                 })}
+              <button
+                className={classNames(
+                  "text-xxs inline-flex items-center font-bold leading-sm uppercase px-3 py-1 rounded-full",
+                  {
+                    "bg-blue-200 text-blue-700": isPendingInvitation,
+                    "bg-red-200 text-red-700": !isPendingInvitation,
+                  }
+                )}
                 onClick={(event) => {
                   event.preventDefault()
                   handleButtonClicked(groupAccessOrPendingInvitation)
-                }}>
+                }}
+              >
                 {isPendingInvitation ? JOIN_BUTTON_LABEL : LEAVE_BUTTON_LABEL}
-                </button>
+              </button>
             )}
-            </div>
+          </div>
         </div>
-        </div>
+      </div>
     </WrapperComponent>
   )
 }
