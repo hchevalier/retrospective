d8d96fef4f80b20310a72aa733abb5992d2a24c7
Hugo Chevalier <hugo.chevalier@doctolib.com>
2021-04-19 22:03:16 +0200

    Fix rubocop offenses

--- app/models/builders/timeline.rb
+++ app/models/builders/timeline.rb
@@ -8,9 +8,10 @@ class Builders::Timeline
 
     def build(retrospective)
       # This uses UTC as application does not have a timezone set
-      weeks_ago = (retrospective.options.dig('weeks_displayed') || 2).to_i.weeks.ago
-      (weeks_ago.beginning_of_week.to_date..Date.today).each do |date|
+      weeks_ago = (retrospective.options['weeks_displayed'] || 2).to_i.weeks.ago
+      (weeks_ago.beginning_of_week.to_date..Time.zone.today).each do |date|
         next if date.on_weekend?
+
         retrospective.add_zone(date.strftime('%d/%m'))
       end
     end
