c47e34d54e6669a3cee228319859de6a59ca5637
Hugo Chevalier <hugo.chevalier@doctolib.com>
2021-04-30 12:15:04 +0200

    Fix a bug

--- app/javascript/components/StepThinking.js
+++ app/javascript/components/StepThinking.js
@@ -35,7 +35,7 @@ const StepThinking = ({ kind, fullScreen, onToggleFullScreen }) => {
     event.stopPropagation()
 
     if (zonesTypology === 'open' || zonesTypology === 'limited') {
-      setSelectedZone(event.target.closest('.zone').dataset.id)
+      setSelectedZone((event.target.closest('.zone') || event.target).dataset.id)
     }
   }
 
