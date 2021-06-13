edb43c04d53b620a5430b7d6dae6a0823ed06523
Hugo Chevalier <hugo.chevalier@doctolib.com>
2021-06-13 22:08:56 +0200

    Move configuration into an YML file

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: Figaro.smtp_username || 'noreply@docto-retrospective.herokuapp.com'
+  default from: Rails.configuration.authentication[:smtp_username]
   layout 'mailer'
 end
